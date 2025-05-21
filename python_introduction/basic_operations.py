353e4809342e2ce2fc4452cd1ac5fcdfcb480259
Elutayo <elutayojohnson@gmail.com>
2025-05-21 03:04:47 +0100

    Add new directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#/usr/bin/env python3
+# This script perfoms basic arithmetic operations.
+
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+print(f"Addition of {number1} and {number2} is: {sum}")
+print(f"Subtraction of {number1} and {number2} is: {difference}")
+print(f"Multiplication of {number1} and {number2} is: {product}")
