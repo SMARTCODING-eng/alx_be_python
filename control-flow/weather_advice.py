d2275196bf5898e277939f749822e9c85f8a26b8
Elutayo <elutayojohnson@gmail.com>
2025-05-30 09:03:25 +0100

    make a correction on my code

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,9 @@
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
 if weather == "sunny":
-    recommendation = "Wear a t-shirt and sunglasses."
+    print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
-    recommendation = "Dont't forget your umbrella and a raincoat."
+    print("Dont't forget your umbrella and a raincoat.")
 elif weather == "cold":
-    recommendation = "Make sure to wear a warm coat and a scarf."
+    print("Make sure to wear a warm coat and a scarf.")
 else:
-    recommendation = "Invalid input."
-print(f"As it is {weather} today {recommendation}")
+    print("Sorry, I don't have recommendations for this weather")
