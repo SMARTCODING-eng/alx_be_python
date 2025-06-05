1f6ae0d581cc1e70261d58ee1fe511cc05bd39f7
Elutayo <elutayojohnson@gmail.com>
2025-06-05 12:43:17 +0100

    adding new file

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,44 @@
+#This is a python script for a shopping store manager that helps in arranging 
+# addind, removing and viewing the current items in the store 
+# This script helps in managing your store
+
+
+# This Code Display the front page and promt user to about what action to take
+def display_menu():
+    print("Shopping List Manager") 
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+#This is the main function that perform the operations 
+# To add items
+# To delete items 
+
+def main():
+    shopping_list = []
+    while True: #This loop throuch the shopping list items to check if its true ofr false
+        display_menu() # To display the menu function above
+        choice = input("Enter your choice: ") 
+
+        if choice == '1':
+            item_name = input("What is the name of the item to be added: ")
+            shopping_list.append(item_name)
+            pass
+        elif choice == '2':
+            item_name == input("Which item do you want to remove: ")
+            shopping_list.remove(item_name)
+            pass
+        elif choice == '3':
+            print(*shopping_list, sep=", ")
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. please try again.")
+
+if __name__ == "__main__":
+    main()
+
+
